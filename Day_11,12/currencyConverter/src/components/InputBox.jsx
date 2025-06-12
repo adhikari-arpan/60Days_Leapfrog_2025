f906decfcdde4578b606e76a834e3d9ac0f55be5
Arpan Adhikari <adhikariarpan2063@gmail.com>
2025-06-12 22:16:52 +0545

    Currency Converter final

--- Day_11,12/currencyConverter/src/components/InputBox.jsx
+++ Day_11,12/currencyConverter/src/components/InputBox.jsx
@@ -1,50 +1,48 @@
-import React from 'react'
-
+import React, {useId} from 'react'
 
 function InputBox({
     label, 
     amount,
     onAmountChange,
     onCurrencyChange,
-    currencyOptions = [],
+    currencyOptions = [], 
     selectCurrency = "usd",
     amountDisable = false,
     currencyDisable = false,
     className = "",
 }) {
    
+const amountInputId = useId();
 
     return (
         <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
             <div className="w-1/2">
-                <label  className="text-black/40 mb-2 inline-block">
+                <label htmlFor={amountInputId}  className="text-black/40 mb-2 inline-block">
                     {label}
                 </label>
                 <input
+                    id={amountInputId}
                     className="outline-none w-full bg-transparent py-1.5"
                     type="number"
                     placeholder="Amount"
-                    disabled = {amountDisable}
+                    disabled={amountDisable}
                     value={amount}
                     onChange={(e) => onAmountChange && onAmountChange(Number(e.target.value))}
-                    //Run onAmountchange() function only when onAmountChange is present
                 />
             </div>
             <div className="w-1/2 flex flex-wrap justify-end text-right">
                 <p className="text-black/40 mb-2 w-full">Currency Type</p>
                 <select
                     className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
-                    value = {selectCurrency}
-                    disabled = {currencyDisable}
+                    value={selectCurrency}
+                    disabled={currencyDisable}
                     onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
                 >
-
-            {/* {currencyOptions.map((currency) => (
-                <option key={} value="usd">
-
-                </option>
-            ))} */}
-                
+                    {currencyOptions.map((currency) => (
+                        <option key={currency} value={currency}>
+                            {currency.toUpperCase()}
+                        </option>
+                    ))}
                 </select>
             </div>
         </div>
--- Day_11,12/currencyConverter/src/hooks/useCurrencyInfo.js
+++ Day_11,12/currencyConverter/src/hooks/useCurrencyInfo.js
@@ -4,12 +4,11 @@ function useCurrencyInfo(currency){
 const [data, setData] = useState({});
 // If no value, then operation will be done on empty object and system doesn't crash. So, empty object will be kept.
 
-    useEffect(()=>{
-        let url = `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`;
+    useEffect(()=>{ 
+        let url = `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`;
         fetch(url)
         .then((res) => res.json()) //Convert the string obtained from API to JSON format
         .then((res) => setData(res[currency]))
-        console.log(data);
     }, [currency])
     return data
 } 
