6e59f4d24d1268f0fc6b27873189118f61678f3b
Arpan Adhikari <adhikariarpan2063@gmail.com>
2025-06-25 22:10:20 +0545

    HTTP Server in Node

---
+++ Day_25/Server/index.js
@@ -0,0 +1,16 @@
+const http = require("http");
+
+const myServer = http.createServer((req, res) => {
+    // console.log(req);
+    console.log("New Request Received");
+    res.end("Hello from server");
+});
+
+myServer.listen(8000, () => {
+    console.log("Server Started");
+});
+
+
+
+
+
