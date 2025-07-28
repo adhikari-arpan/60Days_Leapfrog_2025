dffcdf06494f21472681424f348cf9fa628f4e1f
Arpan Adhikari <adhikariarpan2063@gmail.com>
2025-07-28 18:58:33 +0545

    Day 58

--- Day_56,57,58/index.js
+++ Day_56,57,58/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
+const cookieParser = require('cookie-parser');
 const { connectToMongoDB } = require("./connect");
+const { restrictToLoggedinUserOnly, checkAuth } = require('./middlewares/auth');
 const URL = require('./models/url');
 
 const urlRoute = require("./routes/url");
@@ -19,16 +21,17 @@ app.set("views", path.resolve("./views"));
 //Specifies that view engine is ejs and next specifies views ko file haru views vanney folder ma xa
 
 app.use(express.json());
-app.use(express.urlencoded({estended : false}));
-// Support for form data
+app.use(express.urlencoded({ estended: false }));
+// This middleware helps to support for form data
+app.use(cookieParser());
 
-app.use("/url", urlRoute);
+app.use("/url", restrictToLoggedinUserOnly, urlRoute);
 app.use("/user", userRoute);
-app.use("/", staticRoute);
+app.use("/", checkAuth, staticRoute);
 
 app.get("/test", async (req, res) => {
     const allUrls = await URL.find({});
-    return res.render('home',{
+    return res.render('home', {
         urls: allUrls,
     });
 })
--- Day_56,57,58/middlewares/auth.js
+++ Day_56,57,58/middlewares/auth.js
@@ -1,7 +1,7 @@
 const {getUser} = require("../service/auth");
 
 async function restrictToLoggedinUserOnly(req, res, next){
-    const userUid = req.cookies.uid;
+    const userUid = req.cookies?.uid;
 
     if(!userUid) return res.redirect("/login");
     const user = getUser(userUid);
@@ -12,6 +12,16 @@ async function restrictToLoggedinUserOnly(req, res, next){
     next();
 }
 
+async function checkAuth(req, res, next){
+    const userUid = req.cookies?.uid;
+
+    const user = await getUser(userUid);
+
+    req.user = user;
+    next();
+}
+
 module.exports = {
     restrictToLoggedinUserOnly,
+    checkAuth,
 }
