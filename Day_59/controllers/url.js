691c6b720338a5bcb8eb7fa3acc45409e7d47a7d
Arpan Adhikari <adhikariarpan2063@gmail.com>
2025-07-29 19:09:08 +0545

    Day 59

---
+++ Day_59/controllers/url.js
@@ -0,0 +1,35 @@
+const shortid = require('shortid');
+const model = require('../models/url')
+
+async function handleGenerateNewShortURL(req, res) {
+
+    const body = req.body;
+    if (!body.url) return res.status(400).json({ error: "URL is required" });
+
+    const shortID = shortid();
+    await model.create({
+        shortId: shortID,
+        redirectURL: body.url,
+        visitHistory: [],
+        createdBy: req.user._id,
+    })
+
+    return res.render('home', { id: shortID })
+};
+
+async function handleGetAnalytics(req, res) {
+    const shortId = req.params.shortId;
+    const result = await model.findOne({ shortId });
+    if (!result) {
+        return res.status(404).json({ error: "Short URL not found" });
+    }
+    return res.json({
+        totalClicks: result.visitHistory.length,
+        analytics: result.visitHistory,
+    })
+}
+
+module.exports = {
+    handleGenerateNewShortURL,
+    handleGetAnalytics,
+}
